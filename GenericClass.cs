c65548b9eec452fe5ac432f39b58546e22766f79
venkatesh179 <venkateshdyarangula@gmail.com>
2021-04-15 21:40:16 +0530

    UC1 Added

---
+++ GenericClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1_Generics_To_FindMaxValues
+{
+    public  class GenericClass<T> where T : IComparable
+    {
+        public static int maximum(int x, int y, int z)
+        {
+            int max = x;
+            if (y.CompareTo(max) > 0)
+                max = y;
+            if (z.CompareTo(max) > 0)
+                max = z;
+            return max;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC1_Generics_To_FindMaxValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MaxInteger!");
+           int gc = GenericClass<int>.maximum(2, 14, 6);
+           Console.WriteLine(gc);
+        }
+    }
+
+}
